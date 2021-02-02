4fff3cffb969acbe3b4d11ea6945aab03e810f2b
Anders Aaen Springborg <aaspringborg@gmail.com>
2021-02-02 11:15:41 +0100

    git username and email

---
+++ git.sh
@@ -0,0 +1,4 @@
+GIT_AUTHOR_NAME="Anders Aaen Springborg"
+git config --global user.name "$GIT_AUTHOR_NAME"
+GIT_AUTHOR_EMAIL="aaspringborg@gmail.com"
+git config --global user.email "$GIT_AUTHOR_EMAIL"
