62d1d091fccc91e7c7afd3ead76fadbcc2d6eb19
Anders Aaen Springborg <aaspringborg@gmail.com>
2021-02-02 11:34:30 +0100

    add pause

--- generate_ssh.sh
+++ generate_ssh.sh
@@ -1,5 +1,6 @@
-ssh-keygen -t ed25519 -C "aaspringborg@gmail.com"
-eval "$(ssh-agent -s)" # start the ssh agent in the background
-ssh-add ~/.ssh/id_ed25519
-xclip -selection clipboard < ~/.ssh/id_ed25519.pub #copy the public tag
-echo "Your public ssh key is now added to your clipboard. Go to your host(fx. Github) and paste it"
+ssh-keygen -t ed25519 -C "aaspringborg@gmail.com" ;
+eval "$(ssh-agent -s)" ;# start the ssh agent in the background
+ssh-add ~/.ssh/id_ed25519;
+xclip -selection clipboard < ~/.ssh/id_ed25519.pub; #copy the public tag
+echo "Your public ssh key is now added to your clipboard. Go to your host(fx. Github) and paste it";
+read -p "Press enter to continue";
