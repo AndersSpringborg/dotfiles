c03814203ae0b29624024381a800cedf45bc2d8f
Anders Aaen Springborg <aaspringborg@gmail.com>
2021-02-03 13:55:59 +0100

    renmae

--- bash_options.sh
+++
@@ -1,8 +0,0 @@
-options=(
-    "autocd"   # * `autocd`, e.g. `**/qux` will enter `./foo/bar/baz/qux`
-    "globstar" # * Recursive globbing, e.g. `echo **/*.txt`
-    "cdspell"  # Autocorrect typos in path names when using `cd`
-)
-for option in options; do
-	shopt -s "$option" 2> /dev/null;
-done;
